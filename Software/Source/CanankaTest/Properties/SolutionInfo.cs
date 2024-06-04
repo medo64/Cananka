fafc2e56348eadbcddd3ef5d67a8a009cf95cd3e
Josip Medved <jmedved@jmedved.com>
2024-06-03 19:05:14 -0700

    Moved software to a seaparate repo

--- Software/Source/CanankaTest/Properties/SolutionInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyProduct("Cananka")]
-[assembly: AssemblyCompany("Josip Medved")]
-[assembly: AssemblyCopyright("Copyright (c) 2014 Josip Medved <jmedved@jmedved.com>")]
-
-[assembly: AssemblyVersion("0.00.*")]
-[assembly: AssemblyInformationalVersion("0.00")]
